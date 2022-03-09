d23ecbdc582e43722982d7773325fbcdebd7eacc
Daniel Mehlber <daniel.mehlber@gmail.com>
2022-03-09 20:16:59 +0100

    init commit spring boot

---
+++ src/main/java/org/connectme/core/ConnectMeCoreApplication.java
@@ -0,0 +1,13 @@
+package org.connectme.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectMeCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectMeCoreApplication.class, args);
+	}
+
+}
